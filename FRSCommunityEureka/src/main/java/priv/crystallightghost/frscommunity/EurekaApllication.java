8491e0a4b8328177099f560d0d78ff93b3eeb6a4
CrystallightAir <308690504@qq.com>
2023-03-26 17:50:20 +0800

    包导入优化

---
+++ FRSCommunityEureka/src/main/java/priv/crystallightghost/frscommunity/EurekaApllication.java
@@ -0,0 +1,18 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+/**
+ * @ClassName : FRSCommunityEurekaApllication
+ * @Author : CrystallightAir
+ * @Version : 1.0
+ * @CreateTime : 2023/3/26 16:08
+ * @Description :
+ */
+@SpringBootApplication
+public class EurekaApllication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApllication.class, args);
+    }
+
+}
